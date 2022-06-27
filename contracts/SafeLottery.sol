7b6db24629b00cc1857f90354ce6e37fab47fd35
Joshua Clow <jclowner@icloud.com>
2022-06-27 17:13:58 +0100

    fix: add theoretically safe Lottery contract

---
+++ contracts/SafeLottery.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.8.4;
+import "./libs/Ownable.sol";
+import "./SafeRetrieveRandom.sol";
+
+contract SafeLottery is Ownable {
+    
+    address[] public players; /// Players of the current contest round
+    uint256 quorum; /// Number of participants to automatically trigger the winner picking
+    uint256 feePct; /// Participation fee (in integer percentage)
+    uint256 price; /// Ticket price
+    RetrieveRandom private randGen;
+
+    uint256 revealBlock; /// 
+
+
+    constructor(
+        uint256 _quorum,
+        uint256 _feePct,
+        uint256 _price
+    ) {
+        require(
+            _quorum > 1 && _feePct > 0 && _price > 0,
+            "Initialization parameters failed."
+        );
+        quorum = _quorum;
+        feePct = _feePct;
+        price = _price;
+
+        randGen = new RetrieveRandom();
+    }
+
+    function enter() public payable {
+        require(msg.value >= price);
+        players.push(msg.sender);
+
+        if (players.length >= quorum) revealBlock = block.number;
+    }
+
+    function revealWinner()
+        public
+        payable
+    {
+        require(players.length >= quorum, "This lottery round is still open");
+        // Ensures that the lottery's outcome can not be determined in the same block as the final ticket sale
+        // This protects against contract-based and node-based attacks.
+        // Block producers need to mine 3 blocks in a row to undo their commitment and prevent a losing transaction
+        // from being included in the chain
+        require(block.number + 2 >= revealBlock, "Revealed the result too early");
+         
+        uint256 indexWinner = randGen.getRandomFromRange(revealBlock, 0, players.length - 1);
+        uint256 winnerBalance = ((address(this).balance) * (100 - feePct)) /
+            100;
+        payable(players[indexWinner]).transfer(winnerBalance);
+        payable(__owner).transfer(address(this).balance);
+
+        // Create a new array of players after running pickWinner
+        players = new address[](0);
+    }
+
+    function getRoundPlayers() public view returns (address[] memory) {
+        return players;
+    }
+
+    function getRoundBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function changeConfigs(
+        uint256 _quorum,
+        uint256 _feePct,
+        uint256 _price
+    ) public RequireOwnership {
+        require(
+            players.length == 0,
+            "The execution of this function requires that the current round "
+            "has no player. In order to force proceed with pickWinner before, "
+            "to distribute the current pool."
+        );
+
+        quorum = _quorum;
+        feePct = _feePct;
+        price = _price;
+    }
+}
